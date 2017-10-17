a96f9cbdf6ef3027a384eb6ed7621779560297fa
DerThomy <thomy.koe@googlemail.com>
2017-10-17 15:19:48 +0200

    First Commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Anim minim aute consectetur officia. Incididunt consectetur labore nostrud ullamco et occaecat eu incididunt excepteur ipsum. Excepteur ipsum pariatur voluptate nisi aute quis tempor sit amet labore duis irure. Esse fugiat pariatur minim fugiat elit ullamco eu enim Lorem eu est adipisicing. Ut fugiat deserunt do officia cupidatat.',
+            'author': 'Thomas Köhler',
+            'create_date': '10-15-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Anim minim aute consectetur officia. Incididunt consectetur labore nostrud ullamco et occaecat eu incididunt excepteur ipsum. Excepteur ipsum pariatur voluptate nisi aute quis tempor sit amet labore duis irure. Esse fugiat pariatur minim fugiat elit ullamco eu enim Lorem eu est adipisicing. Ut fugiat deserunt do officia cupidatat.',
+            'author': 'Sahra Köhler',
+            'create_date': '10-15-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Anim minim aute consectetur officia. Incididunt consectetur labore nostrud ullamco et occaecat eu incididunt excepteur ipsum. Excepteur ipsum pariatur voluptate nisi aute quis tempor sit amet labore duis irure. Esse fugiat pariatur minim fugiat elit ullamco eu enim Lorem eu est adipisicing. Ut fugiat deserunt do officia cupidatat.',
+            'author': 'Matthias Köhler',
+            'create_date': '10-15-2017'
+        }
+    ]
+    return articles
